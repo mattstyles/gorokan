ccccb50da11ee733157812faa973aee85ad480fe
Matt Styles <matt@personalurban.com>
2021-12-22 07:50:58 +0000

    update texture management

--- src/goro/texture.ts
+++ src/goro/texture.ts
@@ -5,6 +5,13 @@ import img from '../assets/tiles.png'
 const base = new BaseTexture(img)
 base.scaleMode = SCALE_MODES.NEAREST
 export const textures = new Map<string, Texture>()
+const gridSize = 10
+function getTexture(x: number, y: number): Texture {
+  return new Texture(
+    base,
+    new Rectangle(x * gridSize, y * gridSize, gridSize, gridSize)
+  )
+}
 
 textures.set('goro-hungry', getTexture(0, 0))
 textures.set('goro-free', getTexture(1, 0))
@@ -21,11 +28,3 @@ textures.set('floor-1', getTexture(0, 3))
 textures.set('floor-2', getTexture(1, 3))
 textures.set('floor-red-1', getTexture(2, 3))
 textures.set('floor-red-2', getTexture(3, 3))
-
-const gridSize = 10
-function getTexture(x: number, y: number): Texture {
-  return new Texture(
-    base,
-    new Rectangle(x * gridSize, y * gridSize, gridSize, gridSize)
-  )
-}
--- src/ui/app.tsx
+++ src/ui/app.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react'
-import {Application} from 'pixi.js'
+import {Application, Sprite} from 'pixi.js'
 
 import {state} from '../state/main'
 import {Canvas} from './canvas'
+import {textures} from '../goro/texture'
 
 export function App() {
   return <Canvas onReady={onReady} />
@@ -19,6 +20,12 @@ function onReady({canvas}: {canvas: HTMLCanvasElement}) {
   })
 
   state.app = app
+
+  const sprite = new Sprite(textures.get('goro-free'))
+  sprite.anchor.set(0.5, 0.5)
+  sprite.position.set(20, 20)
+  app.stage.addChild(sprite)
+  sprite.scale.set(4, 4)
 
   return () => {
     app.destroy(true, {
